b115b818b88d6bd286e9a5b5f837e9303bef1b58
Antoine <antoine.marseaud@gmail.com>
2021-01-26 20:12:35 +0100

    ignore

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,39 +1,38 @@
-// Client ID
-// 3xxU-mXwSpIb-gi42rC1xg
-
-// API Key
-// Kc8LcizksnlKCKppWmeVrLqb5i0-Xj4i9wzKW1r5SdSMEC1qzUDLHNeZu0m4fXmVhQCGTMjhvY3ZGdBZPBhyHImBtNtDzL-tJSeRrUOS4U9O-EQMIruoUROnnt6OX3Yx
-
-const apiKey = 'Kc8LcizksnlKCKppWmeVrLqb5i0-Xj4i9wzKW1r5SdSMEC1qzUDLHNeZu0m4fXmVhQCGTMjhvY3ZGdBZPBhyHImBtNtDzL-tJSeRrUOS4U9O-EQMIruoUROnnt6OX3Yx';
+import API_KEY from '../../.env';
+const apiKey = API_KEY;
 
 const Yelp = {
   search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-      headers: {
-        Authorization: `Bearer ${apiKey}`
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`
+        }
       }
-    }).then(response => {
-      return response.json();
-    }).then(jsonResponse => {
-      if(jsonResponse.businesses) {
-        return jsonResponse.businesses.map(business => {
-          console.log(business);
-          return {
-            id: business.id,
-            imageSrc: business.image_url,
-            name: business.name,
-            address: business.location.adress1,
-            city: business.location.city,
-            state: business.location.state,
-            zipCode: business.location.zip_code,
-            category: business.categories[0].title,
-            rating: business.rating,
-            reviewCount: business.review_count
-          }
-        });
-      }
-    })
-    
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => {
+            console.log(business);
+            return {
+              id: business.id,
+              imageSrc: business.image_url,
+              name: business.name,
+              address: business.location.adress1,
+              city: business.location.city,
+              state: business.location.state,
+              zipCode: business.location.zip_code,
+              category: business.categories[0].title,
+              rating: business.rating,
+              reviewCount: business.review_count
+            };
+          });
+        }
+      });
   }
 };
 
